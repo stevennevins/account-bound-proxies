9c69bfaca63d3482a12aa24f455b7834167be4f8
steven nevins <steven.nevins.js@gmail.com>
2023-07-07 10:02:17 -0400

    poc

---
+++ src/Proxy.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Create2} from "openzeppelin-contracts/contracts/utils/Create2.sol";
+
+// https://github.com/Uniswap/v3-core/blob/main/contracts/UniswapV3PoolDeployer.sol
+// https://github.com/Uniswap/v3-periphery/blob/main/contracts/libraries/PoolAddress.sol
+interface ICallbackParams {
+    function params() external returns (address, bytes32);
+}
+
+contract Proxy {
+    /// this will eventually just be a constant
+    bytes32 public immutable initCodeHash;
+    address public immutable deployer;
+
+    constructor() {
+        (address _owner, bytes32 _initCodeHash) = ICallbackParams(msg.sender)
+            .params();
+        require(
+            address(this) ==
+                Create2.computeAddress(
+                    keccak256(abi.encode(_owner)),
+                    _initCodeHash,
+                    msg.sender
+                ),
+            "invalid"
+        );
+        initCodeHash = _initCodeHash;
+        deployer = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(
+            address(this) ==
+                Create2.computeAddress(
+                    keccak256(abi.encode(msg.sender)),
+                    initCodeHash,
+                    deployer
+                ),
+            "not owner"
+        );
+        _;
+    }
+
+    function call() public onlyOwner {}
+}
+
+contract ProxyRegistry {
+    bytes32 public immutable proxyInitCodeHash;
+    address owner;
+
+    constructor() {
+        proxyInitCodeHash = keccak256(type(Proxy).creationCode);
+    }
+
+    function createProxy(address user) external returns (address) {
+        owner = user;
+        address proxy = address(new Proxy{salt: keccak256(abi.encode(user))}());
+        delete owner;
+        return proxy;
+    }
+
+    function params() external returns (address, bytes32) {
+        return (owner, proxyInitCodeHash);
+    }
+}
---
+++ test/Proxy.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "forge-std/Test.sol";
+import "src/Proxy.sol";
+
+contract ProxyTest is Test {
+    Proxy public proxy;
+    bytes32 public initCodeHash;
+    address owner;
+
+    function params() public returns (address, bytes32) {
+        return (owner, initCodeHash);
+    }
+
+    function setUp() public {
+        initCodeHash = keccak256(type(Proxy).creationCode);
+        owner = address(2);
+    }
+
+    function test_Owner() public {
+        bytes32 salt = keccak256(abi.encode(owner));
+        proxy = new Proxy{salt: salt}();
+        vm.prank(address(2));
+        proxy.call();
+    }
+}
