ad10acb095b535e0fff39601dbd033bcfcd5c6e4
steven nevins <steven.nevins.js@gmail.com>
2023-07-15 06:52:02 -0400

    add natspeec

--- src/Router.sol
+++ src/Router.sol
@@ -5,12 +5,18 @@ import {MultiSendCallOnly} from "src/lib/MultiSendCallOnly.sol";
 import {IRegistryCallback} from "src/interfaces/IRegistryCallback.sol";
 import {Proxy} from "openzeppelin-contracts/contracts/proxy/Proxy.sol";
 
+/// @title Router Contract
+/// @notice A contract that acts as a router and proxy for executing multiple transactions.
 contract Router is Proxy {
+    /// @notice The owner of the Router
     address internal immutable owner;
+    /// @notice Option logic that can be installed to enhance functionality of the router
     address internal pluginLogic;
 
+    /// @notice Error when caller is not owner of the router
     error NotOwner();
 
+    /// @dev Modifier to ensure that only the owner can execute a function.
     modifier onlyOwner() {
         if (msg.sender != owner) revert NotOwner();
         _;
@@ -20,10 +26,14 @@ contract Router is Proxy {
         owner = IRegistryCallback(msg.sender).cachedUser();
     }
 
+    /// @notice Updates the plugin logic address.
+    /// @param _pluginLogic The new plugin logic address.
     function updatePluginLogic(address _pluginLogic) external onlyOwner {
         pluginLogic = _pluginLogic;
     }
 
+    /// @notice Executes multiple transactions in a single call.
+    /// @param transactions The byte array containing the encoded transactions.
     function multiSend(bytes memory transactions) external payable onlyOwner {
         MultiSendCallOnly.multiSend(transactions);
     }
@@ -33,7 +43,7 @@ contract Router is Proxy {
             address owner_ = owner;
             assembly {
                 let result := owner_
-                mstore(0x0, result) // store result in memory
+                mstore(0x0, result)
                 return(0x0, 20)
             }
         }
--- src/RouterRegistry.sol
+++ src/RouterRegistry.sol
@@ -6,6 +6,8 @@ import {IRegistryCallback} from "src/interfaces/IRegistryCallback.sol";
 import {IOwner} from "src/interfaces/IOwner.sol";
 import {Create2} from "openzeppelin-contracts/contracts/utils/Create2.sol";
 
+/// @title Router Registry Contract
+/// @notice This contract is used to manage the creation and retrieval of Router contracts
 contract RouterRegistry is IRegistryCallback {
     bytes32 public constant INIT_CODE_HASH = keccak256(type(Router).creationCode);
     address public cachedUser;
@@ -15,6 +17,8 @@ contract RouterRegistry is IRegistryCallback {
     error NotOwner();
     error RouterExists();
 
+    /// @notice Creates a new Router contract for the specified user
+    /// @param user The address of the user for whom the Router contract will be created
     function createRouter(address user) external {
         address router = _predictRouterAddress(keccak256(abi.encode(user)));
         if (router.code.length != 0) revert RouterExists();
@@ -23,16 +27,25 @@ contract RouterRegistry is IRegistryCallback {
         new Router{salt: keccak256(abi.encode(user))}();
     }
 
+    /// @notice Checks if a Router contract exists for the specified user
+    /// @param user The address of the user to check for a Router contract
+    /// @return True if a Router contract exists, false otherwise
     function routerExistsFor(address user) external view returns (bool) {
         address router = _predictRouterAddress(keccak256(abi.encode(user)));
         if (router.code.length == 0) return false;
         return true;
     }
 
+    /// @notice Gets the address of the Router contract for the specified user
+    /// @param user The address of the user for whom to get the Router contract address
+    /// @return The address of the Router contract
     function routerFor(address user) external view returns (address) {
         return _predictRouterAddress(keccak256(abi.encode(user)));
     }
 
+    /// @notice Gets the owner of a Router contract
+    /// @param router The address of the Router contract
+    /// @return The owner address of the Router contract
     function ownerOf(address router) external view returns (address) {
         address routerOwner = IOwner(router).owner();
         if (router != _predictRouterAddress(keccak256(abi.encode(routerOwner)))) revert NotOwner();
