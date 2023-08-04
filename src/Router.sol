00dcb4b4558b2b4d7611c8963548dff3e0f6794f
steven <12021290+stevennevins@users.noreply.github.com>
2023-08-04 08:53:07 -0400

    Support Receiving NFTs Natively (#5)
    * make router nft receiver
    
    * small optimizations

--- src/Router.sol
+++ src/Router.sol
@@ -2,13 +2,17 @@
 pragma solidity ^0.8.20;
 
 import {MultiSendCallOnly} from "src/lib/MultiSendCallOnly.sol";
+import {ERC1155Holder} from "openzeppelin-contracts/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import {ERC721Holder} from "openzeppelin-contracts/contracts/token/ERC721/utils/ERC721Holder.sol";
 import {Proxy} from "openzeppelin-contracts/contracts/proxy/Proxy.sol";
 import {Clones} from "openzeppelin-contracts/contracts/proxy/Clones.sol";
 
 /// @title Router Contract
 /// @notice A contract that acts as a router and proxy for executing multiple transactions.
-contract Router is Proxy {
+contract Router is Proxy, ERC1155Holder, ERC721Holder {
+    using Clones for address;
     /// @notice The router registry
+
     address internal immutable registry = msg.sender;
     /// @notice The router implementation
     address internal immutable routerImplementation = address(this);
@@ -20,7 +24,6 @@ contract Router is Proxy {
 
     /// @notice Updates the plugin logic address.
     /// @param _pluginLogic The new plugin logic address.
-
     function updatePluginLogic(address _pluginLogic) external {
         _checkOwner();
         pluginLogic = _pluginLogic;
@@ -28,7 +31,7 @@ contract Router is Proxy {
 
     /// @notice Executes multiple transactions in a single call.
     /// @param transactions The byte array containing the encoded transactions.
-    function multiSend(bytes memory transactions) external payable {
+    function multiSend(bytes calldata transactions) external payable {
         _checkOwner();
         MultiSendCallOnly.multiSend(transactions);
     }
@@ -38,8 +41,8 @@ contract Router is Proxy {
     }
 
     function _checkOwner() internal view {
-        address router = Clones.predictDeterministicAddress(
-            routerImplementation, keccak256(abi.encode(msg.sender)), registry
+        address router = routerImplementation.predictDeterministicAddress(
+            keccak256(abi.encode(msg.sender)), registry
         );
         if (router != address(this)) revert NotOwner();
     }
